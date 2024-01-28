dac8825048c76168e300fa7d87a4605dd884cd1c
内蒙古射手 <110796157+GQingL@users.noreply.github.com>
2024-01-28 15:52:35 +0800

    Delete src/.umi directory

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/lgq/news/react/react-blog/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','dva','getInitialState','initialStateConfig','request',],
-});
-
-export { plugin };
--- src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from '/Users/lgq/news/react/react-blog/node_modules/react-helmet';
